98537f202ba32f0cb25d7f9c208ee6271bf4131a
SMFahimShahrear <s.m.shahrear.fahim@gmail.com>
2022-04-22 23:43:04 +0600

    About Section Added

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+    $(window).scroll(function()
+    {
+        if(this.scrollY >20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    //toggle menu script
+    $('.menu_btn').click(function()
+    {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu_btn i').toggleClass("active");
+    })
+});
